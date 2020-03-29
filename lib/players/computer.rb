bfe6bc68b0e878ac05b4256f61f7c7547683e12f
Mark Drehmann <markdrehmann@gmail.com>
2020-03-29 20:29:07 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,10 +5,8 @@ module Players
     def move(board)
       all_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       all_moves.sample
-      # available_moves = board.available?
-      # new_moves = all_moves.each_with_index do |move, i|
-      #   move = nil if available_moves[i] == nil
-      # end.sample
+
+      board.taken?(board.position(all_moves.sample))
     end
 
   end
