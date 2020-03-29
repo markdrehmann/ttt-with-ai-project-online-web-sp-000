917adef2ec2b0ca8ec66261a041a1ad9cf5e5efc
Mark Drehmann <markdrehmann@gmail.com>
2020-03-29 20:30:21 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,8 +4,7 @@ module Players
 
     def move(board)
       all_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-      board.taken?(board.position(all_moves.sample))
+      current_move = all_moves.sample if !board.taken?(board.position(current_move))
     end
 
   end
