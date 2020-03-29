21240a01a88a695912dd1e34bbb9e87180789baf
Mark Drehmann <markdrehmann@gmail.com>
2020-03-29 20:33:56 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
   class Computer < Player
 
     def move(board)
-      all_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      all_moves = (1..9).to_a
       current_move = all_moves.sample
       current_move if !@board.taken?(@board.position(current_move))
     end
