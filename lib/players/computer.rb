91cfadcfa3936a9d40258765c6f36df54785ac67
Mark Drehmann <markdrehmann@gmail.com>
2020-03-29 20:21:39 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,11 +4,11 @@ module Players
 
     def move(board)
       all_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      available_moves = board.available?
-      # for each all_moves, check available_moves and if it's nil, make nil in all_moves
-      new_moves = all_moves.each_with_index do |move, i|
-        move = nil if available_moves[i] == nil
-      end.sample
+      all_moves.sample
+      # available_moves = board.available?
+      # new_moves = all_moves.each_with_index do |move, i|
+      #   move = nil if available_moves[i] == nil
+      # end.sample
     end
 
   end
